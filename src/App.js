f1a71e156b77f023f88713f37f706e8ea23acbfe
Michal Siminski <michal.siminski3@gmail.com>
2021-06-03 20:58:30 +0200

    another fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div className="App">asdsad</div>;
+  const [users, setUsers] = useState([]);
+
+  const f = async () => {
+    const res = await fetch("https://reqres.in/api/users/");
+    const json = await res.json();
+    setUsers(json.data);
+    console.log(users);
+  };
+
+  useEffect(() => {
+    f();
+  }, []);
+
+  return <div className="App">{users}</div>;
 }
 
 export default App;
